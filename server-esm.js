668f6ec7f565d53aa5a5e90fc05c8c79a2fe7292
Tum Sirichai <iamtatump@gmail.com>
2025-09-12 21:29:42 +0700

    Update server-esm.js

--- server-esm.js
+++ server-esm.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello! Node.js");
+  res.send("Hello! Node.js (ESM)");
 });
 
 app.get('/users', (req, res) => {
